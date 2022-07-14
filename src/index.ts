6cf08351a1ae224c80d47a6b339f100b6da185e6
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-07-14 21:47:57 +0530

    :sparkles: Basic eventing

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,11 @@ const user = new User({ name: 'myname', age: 20 });
 
 user.set({ name: 'varun' });
 
+user.on('change-name', () => console.log('name changed #1'));
+user.on('change-name', () => console.log('name changed #2'));
+user.on('change-age', () => console.log('age change'));
+
+user.trigger('change-name');
+user.trigger('change-age');
+
 console.log(user.get('name'), user.get('age'));

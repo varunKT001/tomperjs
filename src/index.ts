888357f9b081660887897adf9c9a6bb36c3b9338
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-07-14 19:47:58 +0530

    :sparkles: User model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Project started');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: 'varun' });
+
+console.log(user.get('name'), user.get('age'));
---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
